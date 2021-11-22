70a56dddc8651145cf7679850ac993ad066bbd36
benpaquier <hello@benpaquier.com>
2021-11-22 12:48:15 +0100

    Cours

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import GoogleMapReact from 'google-map-react'
+import styled from 'styled-components'
 
-function App() {
+import Marker from './components/Marker'
+
+const MapContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+`
+
+const App = () => {
+  const [myPosition, setMyPosition] = useState(null)
+
+  useEffect(() => {
+    console.log("Récupération de la localisation...")
+
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        console.log(location.coords)
+        setMyPosition({
+          lat: location.coords.latitude,
+          lng: location.coords.longitude
+        })
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }, [])
+
+  if (!myPosition) {
+    return <p>Chargement...</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MapContainer>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={myPosition}
+        defaultZoom={14}
+      >
+        <Marker
+          lat={myPosition.lat}
+          lng={myPosition.lng}
+        />
+      </GoogleMapReact>
+    </MapContainer>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Marker.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+import { FaMapMarkerAlt } from 'react-icons/fa'
+
+const MarkerContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  position: relative;
+`
+
+const Marker = () => {
+  return (
+    <MarkerContainer>
+      <FaMapMarkerAlt style={{ width: '40px', height: '40px', color: "red", position: "absolute", bottom: '100%', left: '-20px' }} />
+    </MarkerContainer>
+  )
+}
+
+export default Marker
